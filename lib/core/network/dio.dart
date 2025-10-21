8b443995221a0475249fe57b119ad07f69a5a399
reem <reemhossam513@gmail.com>
2025-10-21 03:06:58 +0300

    Complete Registration, Login, and Forgot Password flow UI/Logic.

---
+++ lib/core/network/dio.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+class DioHelper {
+  static late Dio dio;
+
+  static init() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://wekaya.onrender.com/',
+        receiveDataWhenStatusError: true,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      ),
+    );
+  }
+
+  static Future<Response> postData({
+    required String url,
+    required Map<String, dynamic> data,
+    Map<String, dynamic>? query,
+  }) async {
+    return await dio.post(url, data: data, queryParameters: query);
+  }
+}
